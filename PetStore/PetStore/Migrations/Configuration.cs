d7ed86e6347fdae150480cc2d9f46078b285dbca
aaronmbos <aaronmbos@gmail.com>
2018-08-22 19:12:32 -0500

    Add category model

---
+++ PetStore/PetStore/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace PetStore.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PetStore.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "PetStore.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(PetStore.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
