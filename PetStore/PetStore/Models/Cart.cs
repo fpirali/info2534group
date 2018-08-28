2660f89ec19fb653b4ba6bbb0db8e76c29b3048c
aaronmbos <aaronmbos@gmail.com>
2018-08-27 19:44:36 -0500

    Update User view folder.

---
+++ PetStore/PetStore/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetStore.Models
+{
+    public class Cart
+    {
+        public ApplicationUser UserId { get; set; }
+        public List<ProductModels> Products { get; set; }
+        public bool IsPaid { get; set; }
+    }
+}
