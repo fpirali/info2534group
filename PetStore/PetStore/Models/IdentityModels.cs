f7f02ea850ed05cb93a71dd03e7f3c7d927a998b
aaronmbos <aaronmbos@gmail.com>
2018-08-29 19:39:10 -0500

    Update index and add shopping cart controller/view.

--- PetStore/PetStore/Models/IdentityModels.cs
+++ PetStore/PetStore/Models/IdentityModels.cs
@@ -23,11 +23,11 @@ namespace PetStore.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Transaction> Transacations { get; set; }
         public DbSet<ProductModels> ProductModels { get; set; }
         public DbSet<Question> Questions { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
 
         public ApplicationDbContext()
             : base("petstore4", throwIfV1Schema: false)
